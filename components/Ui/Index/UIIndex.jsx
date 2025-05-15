2a706ae9bf76dbd31508f9c57dc889571d003090
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-05-15 14:15:41 +0700

    update

--- components/Ui/Index/UIIndex.jsx
+++ components/Ui/Index/UIIndex.jsx
@@ -33,24 +33,24 @@ export default function UIIndex() {
         </div>
       </div>
       <div className="flex items-center justify-center w-full min-h-[500px] p-2 gap-2 border-2 border-dark border-dashed bg-default rounded-[40px]">
-        <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+        <div className="w-full h-full p-2 border-2 border-dark border-dashed">
           <Swiper
             modules={[Autoplay]}
             pagination={{ clickable: true }}
             autoplay={{ delay: 3000 }}
             loop
             slidesPerView={1}
-            spaceBetween={10}
+            spaceBetween={20}
             breakpoints={{
               640: { slidesPerView: 2, spaceBetween: 20 },
               768: { slidesPerView: 3, spaceBetween: 30 },
               1024: { slidesPerView: 4, spaceBetween: 40 },
             }}
-            className="flex items-center justify-center w-full h-[400px] p-2 gap-2 overflow-auto border-2 border-dark border-dashed"
+            className="w-full h-[400px]"
           >
             {["1", "2", "3", "4", "5", "6", "7", "8", "9"].map((num) => (
               <SwiperSlide key={num}>
-                <div className="flex items-center justify-center w-full h-full p-2 gap-2 bg-white rounded-3xl shadow-md border-2 border-dark border-dashed relative">
+                <div className="w-full h-full p-2 bg-white rounded-3xl shadow-md border-2 border-dark border-dashed relative">
                   <Image
                     src={`/images/floorPlan/floor${num}.png`}
                     alt={`floor${num}`}
